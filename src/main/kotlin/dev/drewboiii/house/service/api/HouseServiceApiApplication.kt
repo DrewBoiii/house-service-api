c5a989cde9e56289809c1c44d3392c879347ba39
drewboiii <goodchickenboy@mail.ru>
2025-11-08 18:12:48 +0400

    Initial commit

---
+++ src/main/kotlin/dev/drewboiii/house/service/api/HouseServiceApiApplication.kt
@@ -0,0 +1,11 @@
+package dev.drewboiii.house.service.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HouseServiceApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<HouseServiceApiApplication>(*args)
+}
---
+++ src/test/kotlin/dev/drewboiii/house/service/api/HouseServiceApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.drewboiii.house.service.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HouseServiceApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
